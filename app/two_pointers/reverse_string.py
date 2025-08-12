846c9f73814449b99222bdfc8bdfebd84a703c68
georgelouis <george.louis@meltwater.com>
2025-08-12 23:29:42 +0530

    added binary search

--- app/two_pointers/reverse_string.py
+++ app/two_pointers/reverse_string.py
@@ -1,3 +1,13 @@
 def reverse_string(s: str):
     s_list = [i for i in s]
-    pass
+    i = 0
+    j = len(s_list) - 1
+
+    while i < j:
+        s_list[i], s_list[j] = s_list[j], s_list[i]
+        i += 1
+        j -= 1
+    return s_list
+
+
+print(reverse_string("there"))
