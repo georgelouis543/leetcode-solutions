fdd9b1063d19bdc6c5de05346534f557e1684f58
georgelouis <george.louis@meltwater.com>
2025-08-21 22:23:53 +0530

    added leetcode 189

---
+++ app/general_array_problems/rotate_array_constant_space_189.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        if k > len(nums):
+            k = k % len(nums)
+
+        i = 0
+        j = len(nums) - 1
+        while i <= j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        print(nums)
+
+        i = 0
+        j = k - 1
+        while i <= j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        print(nums)
+
+        i = k
+        j = len(nums) - 1
+        while i <= j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+
+        return None
