c9b53b517af5430bc252e9af4946367b46719366
georgelouis <george.louis@meltwater.com>
2025-08-26 15:23:43 +0530

    added leetcode 70

---
+++ app/dynamic_programming/climbing_stairs_70.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # memoization
+        memo = {1: 1, 2: 2}
+        def f(x):
+            if x in memo:
+                return memo[x]
+            else:
+                memo[x] = f(x - 1) + f(x - 2)
+                return memo[x]
+
+        return f(n)
