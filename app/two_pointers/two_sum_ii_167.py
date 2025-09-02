bafd39f55b7397ed070b940d33ba26421192c98c
georgelouis <george.louis@meltwater.com>
2025-09-02 11:10:21 +0530

    added leetcode 167

---
+++ app/two_pointers/two_sum_ii_167.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        curr_sum = 0
+
+        while l < r:
+            curr_sum = numbers[l] + numbers[r]
+
+            if curr_sum == target:
+                return [l + 1, r + 1]
+
+            elif curr_sum < target:
+                l += 1
+
+            else:
+                r -= 1
+
+        return []
