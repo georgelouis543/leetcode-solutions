6db47c43bb851c36aa2bb2ceecd2d65766b4c550
georgelouis <george.louis@meltwater.com>
2025-09-04 16:01:34 +0530

    added leetcode 1509

---
+++ app/two_pointers/minimum_difference_in_three_moves_1509.py
@@ -0,0 +1,18 @@
+from typing import List
+# O(n*logn) time complexity
+
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        min_diff = float('inf')
+        print(nums)
+
+        if len(nums) <= 4:
+            return 0
+
+        for l in range(0, 4):
+            r = len(nums) - 4 + l
+
+            min_diff = min(min_diff, nums[r] - nums[l])
+
+        return min_diff
