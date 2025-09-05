7390aad165d395bb9365d43ec13455fcdbf7f586
georgelouis <george.louis@meltwater.com>
2025-09-05 23:30:39 +0530

    added leetcode 88

---
+++ app/two_pointers/merge_sorted_arrays_88.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = m + n - 1
+        k = n - 1
+
+        while k >= 0:
+
+            if i >= 0 and nums1[i] > nums2[k]:
+                nums1[j] = nums1[i]
+                i -= 1
+                j -= 1
+
+            else:
+                nums1[j] = nums2[k]
+                k -= 1
+                j -= 1
+
+        return nums1
+
+
