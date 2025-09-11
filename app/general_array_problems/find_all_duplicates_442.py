1caef41f9b6f93cdadbf4fa0b65cb1ba973ac910
georgelouis <george.louis@meltwater.com>
2025-09-11 22:30:24 +0530

    added leetcode 442 (constant space)

---
+++ app/general_array_problems/find_all_duplicates_442.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+
+        for num in nums:
+            num = abs(num)
+
+            if nums[num - 1] < 0:
+                res.append(num)
+
+            nums[num - 1] = -nums[num - 1]
+
+        return res
