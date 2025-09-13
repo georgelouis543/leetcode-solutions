1ca642498c878463bc7a205f14f1daeb72517a6a
georgelouis <george.louis@meltwater.com>
2025-09-13 22:43:46 +0530

    added leetcode 2108

---
+++ app/string_manipulation/find_first_palindrome_2108.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+
+        def is_palindrome(word: str) -> bool:
+            l = 0
+            r = len(word) - 1
+            while l < r:
+                if word[l] != word[r]:
+                    return False
+                l += 1
+                r -= 1
+
+            return True
+
+        for word in words:
+            if is_palindrome(word):
+                return word
+
+        return ""
